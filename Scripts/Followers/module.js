081571081a149b9bd81afed9444ce2343414e2e9
striderwhite <re6ter@gmail.com>
2017-08-09 14:20:01 -0600

    created recursive call using promise methology to get series of followers from GET requests

--- Scripts/Followers/module.js
+++ Scripts/Followers/module.js
@@ -20,40 +20,56 @@ var app = angular.module("followersApp", []).config(function ($sceDelegateProvid
 //Controller only concerned with getting followers from SC API
 app.controller("followersController", function ($scope, $http, $q, _) {
 
+    //-----------------------//
+    //         SETUP         //    
+    //-----------------------//
 
-
+    $scope.SCEndpoint = "https://api-v2.soundcloud.com/users/2751638/followers?offset=1501351687918&limit=200&client_id=JlZIsxg2hY5WnBgtn3jfS0UYCl0K8DOg";
     //Set up array of followers
     $scope.followers = [];
     //Lodash
     $scope._ = _;
 
-    //Make a promise to get data from SC API
-    var promise = getFollowers($q, $http, $scope, proxy + scURL);
-    promise.then((data) => {
+    //set up sorting stuff
+    $scope.sortColumn = "followers_count"
+    $scope.reverseSort = true;
+    $scope.sortData = function (column) {
+        //$scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+        $scope.sortColumn = column
+    }
 
-        //_ = _.noConflict(); // lets call ourselves _u
-        //_.merge($scope.followerCount, data.collection);
+    //end sorting stuff
 
-        //$scope.followers = $scope.followers.concat(data.collection);
+    //-----------------------//
+    //         FUNCS         //    
+    //-----------------------//
+    
 
-        console.log("Attempt INIT merg. Before: " + $scope.followers.length);
-        _.merge($scope.followers, data.collection);
-        console.log("Attempt INIT merg. After: " + $scope.followers.length);
+    //Get the first set of followers with a promise
+    $scope.start = function()
+    {
+        //Make a promise to get data from SC API
+        var promise = getFollowers($q, $http, $scope, proxy + $scope.SCEndpoint);
+        promise.then((data) => {
 
-        $scope.followerCount = $scope.followers.length;
-        $scope.next_href = data.next_href;
+            //get the first set of followers
+            _.merge($scope.followers, data.collection);
+            $scope.followerCount = $scope.followers.length;
+            $scope.next_href = data.next_href;
+
+        //Now, depending of next_href is null or not, keep call "next page".
+        }).then((response)=>
+        {
+            if($scope.next_href != null) {
+                nextPage();
+            }
+        });
+
+    }
 
 
-
-        //console.log(JSON.stringify($scope.followers));
-        //console.log("----------------------------------------------------------------------------------------------------------------------------------------------");
-        //console.log(JSON.stringify(data.collection));
-
-    });
-
-
-
-    $scope.nextPage = function nextPage() {
+    //This function keeps calling itself after a promise resolve until that promise rejects
+    function nextPage() {
         console.log("Getting next page");
         var promise2 = getFollowers($q, $http, $scope, proxy + $scope.next_href + token);
         promise2.then((data) => {
@@ -68,23 +84,13 @@ app.controller("followersController", function ($scope, $http, $q, _) {
             $scope.followerCount = $scope.followers.length;
             $scope.next_href = data.next_href;
 
+            if (data.next_href != null) {
+                nextPage();
+            }
 
-            //console.log(JSON.stringify($scope.followers));
-            //console.log("----------------------------------------------------------------------------------------------------------------------------------------------");
-            //console.log(JSON.stringify(data.collection));
-
-            //console.log("$scope.followers before: " + $scope.followers.length);
-            //console.log("data before: " + $scope.followers.length);
-
-            //angular.extend($scope.followers, data.collection);
-            //console.log("$scope.followers after: " + $scope.followers.length);
-            //console.log("data after: " + $scope.followers.length);
-
-            //$scope.followers.concat(data.collection);
-
-            //Object.assign($scope.followers, data.collection);
-            //$scope.followerCount = $scope.followers.count;
-            //$scope.next_href = data.next_href;
+        //We hit here when promise is rejected
+        }, (err) => {
+            $scope.next_href = "Done! :)";
         });
     }
 
