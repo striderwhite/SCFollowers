3f13a629d4eebace40914165f019144bc21aa951
striderwhite <re6ter@gmail.com>
2017-08-09 00:42:26 -0600

    'overhaul'

---
+++ Scripts/Followers/followers.js
@@ -0,0 +1,79 @@
+﻿/// <reference path="angular.js" />
+var scURL = "https://api-v2.soundcloud.com/users/2751638/followers?offset=1501351687918&limit=200&client_id=JlZIsxg2hY5WnBgtn3jfS0UYCl0K8DOg";
+//var scURL = "https://api-v2.soundcloud.com/users/270369011/followers?client_id=JlZIsxg2hY5WnBgtn3jfS0UYCl0K8DOg&limit=200&offset=0"
+//CORS proxy
+var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+
+//create module then register sc API as a safe source
+//see https://stackoverflow.com/questions/41642646/angularjs-errors-blocked-loading-resource-from-url-not-allowed-by-scedelegate
+var app = angular.module("followersApp", []).config(function ($sceDelegateProvider,$httpProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'https://api-v2.soundcloud.com/**',
+        'https://api.soundcloud.com/**'
+    ]);
+    $httpProvider.defaults.useXDomain = true;
+});
+
+app.controller("followersController",function ($scope, $http, $q) {
+
+
+});
+
+
+
+function getNextSetOfFollowers($scope, $http) {
+    console.log("Calling " + $scope.next_href);
+    $http.get(proxy + $scope.next_href, {
+        method: 'GET'
+    }).then(function success(response) {
+        $scope.next_href = response.data.next_href;
+        $scope.URLS.push(response.data.next_href);
+        return response.data.next_href == null;
+    });
+}
+
+
+
+function buildListOfURL($scope, $http, $q) {
+    var next_href = scURL;
+    $scope.URLS = [];
+
+    do {
+        console.log("Calling " + next_href);
+        $http.get(proxy + next_href, {
+            method: 'GET'
+        }).then(function success(response) {
+            $scope.URLS.push(response.data.next_href);
+            $scope.next_href = response.data.next_href;
+        });
+    } while (next_href != null)
+
+}
+
+
+function getFollowers($scope, $http) {
+
+
+
+    $http.get(proxy + scURL, {
+        method: 'GET'
+    }).then(function successCallback(response) {
+
+        //keep appending data until done
+        while ($scope.next_href != null)
+        {
+
+            $scope.users += response.data;
+            $scope.next_href = response.data.next_href;
+        }
+
+
+
+
+        //ERROR HANDLE
+    }, function errorCallback(response) {
+        $scope.users = response.data;
+    });
+}
---
+++ Scripts/Followers/module.js
@@ -0,0 +1,93 @@
+﻿/// <reference path="APICaller.js" />
+
+//Hard coded url to api call
+var scURL = "https://api-v2.soundcloud.com/users/2751638/followers?offset=1501351687918&limit=200&client_id=JlZIsxg2hY5WnBgtn3jfS0UYCl0K8DOg";
+//CORS proxy
+var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+var token = '&client_id=JlZIsxg2hY5WnBgtn3jfS0UYCl0K8DOg';
+
+var app = angular.module("followersApp", []).config(function ($sceDelegateProvider, $httpProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'https://api-v2.soundcloud.com/**',
+        'https://api.soundcloud.com/**'
+    ]);
+    $httpProvider.defaults.useXDomain = true;
+}).constant('_', window._);
+
+
+//Controller only concerned with getting followers from SC API
+app.controller("followersController", function ($scope, $http, $q, _) {
+
+
+
+    //Set up array of followers
+    $scope.followers = [];
+    //Lodash
+    $scope._ = _;
+
+    //Make a promise to get data from SC API
+    var promise = getFollowers($q, $http, $scope, proxy + scURL);
+    promise.then((data) => {
+
+        //_ = _.noConflict(); // lets call ourselves _u
+        //_.merge($scope.followerCount, data.collection);
+
+        //$scope.followers = $scope.followers.concat(data.collection);
+
+        console.log("Attempt INIT merg. Before: " + $scope.followers.length);
+        _.merge($scope.followers, data.collection);
+        console.log("Attempt INIT merg. After: " + $scope.followers.length);
+
+        $scope.followerCount = $scope.followers.length;
+        $scope.next_href = data.next_href;
+
+
+
+        //console.log(JSON.stringify($scope.followers));
+        //console.log("----------------------------------------------------------------------------------------------------------------------------------------------");
+        //console.log(JSON.stringify(data.collection));
+
+    });
+
+
+
+    $scope.nextPage = function nextPage() {
+        console.log("Getting next page");
+        var promise2 = getFollowers($q, $http, $scope, proxy + $scope.next_href + token);
+        promise2.then((data) => {
+            console.log("Got " + data.collection.length + " new followers");
+
+            console.log("Attempt merg. Before: " + $scope.followers.length);
+            //_.merge($scope.followers, data.collection);
+            $scope.followers = $scope.followers.concat(data.collection);
+            console.log("Attempt merg. After: " + $scope.followers.length);
+
+
+            $scope.followerCount = $scope.followers.length;
+            $scope.next_href = data.next_href;
+
+
+            //console.log(JSON.stringify($scope.followers));
+            //console.log("----------------------------------------------------------------------------------------------------------------------------------------------");
+            //console.log(JSON.stringify(data.collection));
+
+            //console.log("$scope.followers before: " + $scope.followers.length);
+            //console.log("data before: " + $scope.followers.length);
+
+            //angular.extend($scope.followers, data.collection);
+            //console.log("$scope.followers after: " + $scope.followers.length);
+            //console.log("data after: " + $scope.followers.length);
+
+            //$scope.followers.concat(data.collection);
+
+            //Object.assign($scope.followers, data.collection);
+            //$scope.followerCount = $scope.followers.count;
+            //$scope.next_href = data.next_href;
+        });
+    }
+
+});
+
+
